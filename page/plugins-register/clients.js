32e37d5c61cda4fbc7eb6a9ceba18c3bf8e08c9c
JoseDeJesus002 <jose_1321124119@uptecamac.edu.mx>
2022-06-03 18:44:23 -0500

    Point vent

--- page/plugins-register/clients.js
+++ page/plugins-register/clients.js
@@ -13,11 +13,17 @@ function addClientToSystem(pname,pedad,puser,pemail,psexo,ppass){
     clientList.push(newClient);
     localStorageClientList(clientList);
 }
-/*
+
 function getClientList(){
+    var storedList=localStorage.getItem('localClientList')
+    if(storedList == null){
+        clientList = [];
+    }else{
+        clientList = JSON.parse(storedList);
+    }
     return clientList;
 }
-*/
+
 
 function localStorageClientList(pList){
     localStorage.setItem('localClientList', JSON.stringify(pList));
