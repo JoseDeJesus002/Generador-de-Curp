d068de705f3745c28524c76c6b7ffa277de3e0bb
JosedeJesus002 <jose_1321124119@uptecamac.edu.mx>
2022-11-01 23:07:21 -0600

    V02

--- Programacion_Web/Programas_Web/Ticket/scripts/script_from.js
+++ Programacion_Web/Programas_Web/Ticket/scripts/script_from.js
@@ -1,32 +1,50 @@
 //almacenar respuestas correctas en el vector
-var repsuestaCorrecta = new Array("a1", "b1" ,"c3" ,"d3","f3");
+var respuestaCorrecta = [
+  "a3",
+  "b2",
+  "c1",
+  "d3",
+  "e1",
+  "f3",
+  "g2",
+  "h2",
+  "i3",
+  "j3",
+];
+var img = document.getElementById("correct");
+var img2 = document.getElementById("error");
 
-function comprobarRadio(vector)
-{
-    var respuestasBuenas=0;
-    var respuestasMalas=0;
+function comprobarRadio(vector) {
+  var respuestasBuenas = 0;
+  var respuestasMalas = 0;
 
-    for(j=0;j< vector.length; j++)
-    {
-        var radios = document.getElementsByName(j+1);
-        for(i=0; i< radios.length; i++)
-        {
-            if(radios[i].checked)
-            {
-                var valor= radios[i].value;
-                if(valor == vector[j] )
-                {
-                    respuestasBuenas++;
-                    break;
-                }else
-                {
-                    respuestasMalas++;
-                    break
-                }
-            }
+  for (j = 0; j < vector.length; j++) {
+    var radios = document.getElementsByName(j + 1);
+    for (i = 0; i < radios.length; i++) {
+      if (radios[i].checked) {
+        var valor = radios[i].value;
+        if (valor == vector[j]) {
+        img.style.visibility = "visible";
+        img2.style.visibility = "hidden";  
+        respuestasBuenas++;
+          break;
+        } else {
+            img2.style.visibility = "visible";
+            img.style.visibility = "hidden";
+          respuestasMalas++;
+          break;
         }
+      }
     }
-    document.getElementById("resultasdos").innerHTM=" Respuestas correctas" + " "+ "<b>" + respuestasBuenas + "</b>" + "</br>" + "Respuestas malas"+ " " + respuestasMalas;
+  }
+  document.getElementById("resultados").innerHTML =
+    " Respuestas correctas" +
+    " " +
+    "<b>" +
+    respuestasBuenas +
+    "</b>" +
+    "</br>" +
+    "Respuestas malas" +
+    " " +
+    respuestasMalas;
 }
-
-
